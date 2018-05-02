d3af12c4b5fc1b283f3c5c7e44b3ef92f3e1a5e4
Saleem Hadad <saleem_ha@hotmail.com>
2018-05-02 21:38:17 +0800

    add HomeViewController

---
+++ FIREAIOT/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  HomeViewController.swift
+//  FIREAIOT
+//
+//  Created by Saleem Hadad on 02/05/2018.
+//  Copyright © 2018 Binary Torch. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
